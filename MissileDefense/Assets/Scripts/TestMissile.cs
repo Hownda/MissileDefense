d118329fad4c2773a6ee6a6d095dd5e074f9e364
Hownda <117111277+Hownda@users.noreply.github.com>
2023-11-10 16:25:30 +0100

    Added thrust and drag

--- MissileDefense/Assets/Scripts/TestMissile.cs
+++ MissileDefense/Assets/Scripts/TestMissile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,13 @@ public class TestMissile : MonoBehaviour
 {
     public bool useGravity = true;
 
-    public float thrustForce = 50f;
+    public float thrustForce = 10f;
     public float gravityForce = 9.81f;
+    public float dragCoefficient = 0.04f;
+    public float diameter = 0.2f;
+    public float airDensity = 1.3f;
+
+    private Vector3 resultingForce;
 
     Transform missilePos;
     Rigidbody rb;
@@ -20,7 +26,36 @@ public class TestMissile : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 force = new(transform.forward.x * thrustForce, useGravity ? gravityForce : transform.forward.y * thrustForce, transform.forward.z * thrustForce);
-        rb.AddForce(force * Time.deltaTime);
+        ApplyThrust();
+        ApplyDrag();
+        //ApplyFriction();
+        //ApplyGravity();
+        //Vector3 force = new(transform.forward.x * thrustForce, useGravity ? gravityForce : transform.forward.y * thrustForce, transform.forward.z * thrustForce);
+        //rb.AddForce(force * Time.deltaTime);       
     }
+
+    private void ApplyGravity()
+    {
+        throw new NotImplementedException();        
+    }
+
+    private void ApplyFriction()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ApplyThrust()
+    {
+        resultingForce = transform.forward * thrustForce;
+    }
+
+    private void ApplyDrag()
+    {
+        float area = Mathf.Pow(diameter / 2, 2) * Mathf.PI;
+        Vector3 airResistance = dragCoefficient * area * airDensity * rb.velocity.sqrMagnitude * -rb.velocity.normalized / 2;
+        resultingForce += airResistance;
+        rb.AddForce(resultingForce);
+    }
+
+
 }
